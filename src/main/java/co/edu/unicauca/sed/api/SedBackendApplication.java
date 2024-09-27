7e0f42815b17067873fc29ea42a9c9387db33cda
bdperdomo <bdperdomo@unicauca.edu.co>
2024-09-27 08:53:52 -0500

    Configuracion inicial del proyecto

---
+++ src/main/java/co/edu/unicauca/sed/api/SedBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.sed.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/sed/api/SedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.sed.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
