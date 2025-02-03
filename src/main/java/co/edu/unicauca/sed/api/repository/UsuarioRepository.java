593e15ec7fec0211d7ab5cab47680fe1fd1c6bcd
jhayber17 <jhayberm@unicauca.edu.co>
2025-02-02 23:20:04 -0500

    Feature validacion jefe departamento

--- src/main/java/co/edu/unicauca/sed/api/repository/UsuarioRepository.java
+++ src/main/java/co/edu/unicauca/sed/api/repository/UsuarioRepository.java
@@ -1,11 +1,10 @@
 package co.edu.unicauca.sed.api.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import co.edu.unicauca.sed.api.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>, JpaSpecificationExecutor<Usuario> {
     Usuario findByIdentificacion(String identificacion);
-    List<Usuario> findByRoles_Nombre(String nombreRol);
+    boolean existsByRolesNombre(String nombreRol);
 }
